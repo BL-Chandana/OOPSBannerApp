5d42bff790ad922b00142289dc5e47e4c96ef0f8
BL-Chandana <chandana.sree@bridgelabz.com>
2026-01-05 12:21:15 +0530

    [Chandana]-feat(UC5): Inline Array Initialization

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,20 @@
 /**
- * UC4: Banner using String Array and Loop
- * it stores all banner lines first and then prints them using a loop.
+ * UC5: Inline Array Initialization
+ * UC5 stores all banner rows together in an array and prints them using a loop.
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = new String[7];
-
-        banner[0] = String.join(" ", " ******** ", " ******** ", " ******** ", " ******** ");
-        banner[1] = String.join(" ", "*        *", "*        *", "*        *", "*         ");
-        banner[2] = String.join(" ", "*        *", "*        *", "*        *", "*         ");
-        banner[3] = String.join(" ", "*        *", "*        *", " ******** ", " ******** ");
-        banner[4] = String.join(" ", "*        *", "*        *", "*         ", "         *");
-        banner[5] = String.join(" ", "*        *", "*        *", "*         ", "         *");
-        banner[6] = String.join(" ", " ******** ", " ******** ", "*         ", " ******** ");
+        String[] banner = {
+            String.join(" ", " ******** ", " ******** ", " ******** ", " ******** "),
+            String.join(" ", "*        *", "*        *", "*        *", "*         "),
+            String.join(" ", "*        *", "*        *", "*        *", "*         "),
+            String.join(" ", "*        *", "*        *", " ******** ", " ******** "),
+            String.join(" ", "*        *", "*        *", "*         ", "        * "),
+            String.join(" ", "*        *", "*        *", "*         ", "        * "),
+            String.join(" ", " ******** ", " ******** ", "*         ", " ******** ")
+        };
 
         for (String line : banner) {
             System.out.println(line);
